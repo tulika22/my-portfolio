9b94f67fcc61b9c3864c9d4ea6e5d4c5f82316eb
Tulika Tiwari <tulikatiwari@Tulikas-MacBook-Pro.local>
2025-05-21 01:56:31 +0530

    fix navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -17,9 +17,9 @@ export const Navbar = () => {
 
   useEffect(() => {
     const handleScroll = () => {
-      setIsScrolled(window.screenY > 10);
+      setIsScrolled(window.scrollY > 10); 
     };
-
+  
     window.addEventListener("scroll", handleScroll);
     return () => window.removeEventListener("scroll", handleScroll);
   }, []);
